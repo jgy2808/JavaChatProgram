bf1405d9a0c41ed1a293cb2540f87a95974dd90c
jgy2808 <jgy2808@naver.com>
2021-07-06 16:31:41 +0900

    Cloning open source
    Set jframe(chatting room frame), login1(login frame).
    Build actionPerformed method.

--- ChatProgram/src/ChatProgram/MultiClient.java
+++ ChatProgram/src/ChatProgram/MultiClient.java
@@ -111,23 +111,24 @@ public class MultiClient implements ActionListener {
 		jframe.add(jp1, BorderLayout.SOUTH); // 채팅창 하단 입력tf, 전송btn
 		jframe.add(jp2, BorderLayout.NORTH); // 채팅창 상단 채팅방이름, ip
 		login1.add(jp3, BorderLayout.EAST); // 로그인 화면 gridlayout 전부
-		login1.add(jp4, BorderLayout.EAST); // 뭔지 모르겠음
+		login1.add(jp4, BorderLayout.EAST); // 뭔지 모르겠음 질문1 : 무슨 Panel인지
 		
 		JScrollPane jsp = new JScrollPane(jta, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
 				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		jframe.add(jsp, BorderLayout.CENTER); // 채팅창 중간 키티화면의 채팅 내역ta
 		JScrollPane jsp1 = new JScrollPane(jlo, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
 				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-		login1.add(jp3, BorderLayout.CENTER);
+		login1.add(jp3, BorderLayout.CENTER); // 질문2 : jsp1(jlo)의 위치, 역할 | jp3은 위에서 add시켜줬는데 왜 또 해야하나
 		
 		jtf.addActionListener(this);
-		jbtn.addActionListener(this);
-		jexit.addActionListener(this);
+		jbtn.addActionListener(this); // 채팅창에서 enter btn
+		jbtn1.addActionListener(this); // 로그인창에서 login btn
+		jexit.addActionListener(this); // 로그인창에서 exit btn
 		
 		jframe.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				try {
-					oos.writeObject(id + "#exit");
+					oos.writeObject(id + "#exit"); // id가 어디서 초기화되는지 보기 -> actionPerformed -> if action Login btn
 				} catch (IOException ee) {
 					ee.printStackTrace();
 				}
@@ -139,9 +140,78 @@ public class MultiClient implements ActionListener {
 			}
 		});
 		
+		jta.setEditable(false);
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		Dimension d = tk.getScreenSize();
+		int screenHeight = d.height;
+		int screenWidth = d.width;
 		
+		jframe.pack();
+		jframe.setLocation((screenWidth = jframe.getWidth()) / 2, (screenHeight - jframe.getHeight()) / 2);
+		jframe.setResizable(false);
+		jframe.setVisible(false);
+		
+		login1.pack();
+		login1.setSize(800, 300);
+		login1.setLocation((screenWidth = jframe.getWidth()) / 2, (screenHeight - jframe.getHeight()) / 2);
+		login1.setResizable(false);
+		login1.setVisible(true);		
 	}
 	
+	public void actionPerformed(ActionEvent e) {
+		Object obj = e.getSource();
+		String msg = jtf.getText();
+		
+		String str = e.getActionCommand();
+		
+		if (str.equals("Login")) {
+			jframe.setVisible(true);
+			login1.setVisible(false);
+			
+			ip = idc.getText();
+			id = pass.getText();
+		}
+		if (str.equals("exit")) {
+			System.exit(0);
+		}
+		if (obj == jtf && changepower == true) {
+			changepower = false;
+			if (msg == null || msg.length() == 0) { // 채팅 메시지 입력이 없을때 
+				JOptionPane.showMessageDialog(jframe, "글을 쓰세요", "경고", JOptionPane.WARNING_MESSAGE);
+			} else {
+				id = jtf.getText();
+				jtf.setText("");
+			}
+		} else if (obj == jtf && saypower == true) {
+			saypower = false;
+			if (msg == null || msg.length() == 0) {
+				JOptionPane.showMessageDialog(jframe, "글을 쓰세요", "경고", JOptionPane.WARNING_MESSAGE);
+			} else {
+				id = jtf.getText();
+				jtf.setText("");
+			}
+		}
+		
+		if (obj == jtf) { // jtf에서 enter가 눌렸을 때
+			if (msg == null || msg.length() == 0) {
+				JOptionPane.showMessageDialog(jframe, "글을 쓰세요", "경고", JOptionPane.WARNING_MESSAGE);
+			} else {
+				try {
+					oos.writeObject(id + "#" + msg); // 질문3 : 위에 oos.writeObject(id + "#exit") 도 그렇고 이게 어디에 저장되는 것인가??
+				} catch (IOException ee) {
+					ee.printStackTrace();
+				}
+				jtf.setText("");
+			}
+		} else if (obj == jbtn) { // enter btn -> 누르면 종료됨
+			try {
+				oos.writeObject(id + "#exit");
+			} catch (IOException ee) {
+				ee.printStackTrace();
+			}
+			System.exit(0); // 이거 때문에 처음 시연해볼 때 채팅 입력창 옆 enter 버튼 누르면 종료했던 것
+		}
+	}
 	
 	public static void main(String[] args) {
 		
