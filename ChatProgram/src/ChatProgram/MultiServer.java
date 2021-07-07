74d607b769483392118f8642c85e30ae8fdfdb99
jgy2808 <jgy2808@naver.com>
2021-07-07 19:33:39 +0900

    Cloning open source
    Complete clone MultiServer.java.
    Use ArrayList | add().
    Use Socket, ServerSocket class.

--- ChatProgram/src/ChatProgram/MultiServer.java
+++ ChatProgram/src/ChatProgram/MultiServer.java
@@ -1,9 +1,41 @@
 package ChatProgram;
 
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 public class MultiServer {
-
-	public static void main(String[] args) {
-
+	private ArrayList<MultiServerThread> list;
+	private Socket socket;
+	
+	public MultiServer() throws IOException {
+		list = new ArrayList<MultiServerThread>();
+		ServerSocket serverSocket = new ServerSocket(5000);
+		MultiServerThread mst = null;
+		boolean isStop = false;
+		while (!isStop) {
+			System.out.println("Server ready...");
+			socket = serverSocket.accept();
+			mst = new MultiServerThread(this);
+			list.add(mst);
+			Thread t = new Thread(mst);
+			t.start();
+		}
+	}
+	
+	public void Ddos() {
+		System.exit(0);
+	}
+	
+	public ArrayList<MultiServerThread> getList() {
+		return list;
+	}
+	
+	public Socket getSocket() {
+		return socket;
 	}
 
+	public static void main(String[] args) throws IOException {
+		new MultiServer();
+	}
 }
